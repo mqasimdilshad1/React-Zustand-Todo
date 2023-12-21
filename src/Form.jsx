be5c77b649c78496c69a30031109af0722b9dda7
Muhammad Qasim Dilshad <qasimdilshad336@gmail.com>
2023-12-21 17:55:29 +0500

    favorite functionality added

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,10 @@
 import useTodoStore from "./Store";
 import { useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
+import { FaTrashAlt, FaHeart, FaRegHeart } from "react-icons/fa";
+
 const Form = () => {
   const [newTodo, setNewTodo] = useState("");
-  const { todos, addTodo, deleteTodo } = useTodoStore();
+  const { todos, addTodo, deleteTodo, toggleFavorite } = useTodoStore();
 
   const handleAddTodo = (e) => {
     e.preventDefault();
@@ -12,21 +13,24 @@ const Form = () => {
       setNewTodo("");
     }
   };
+
   return (
-    <div className="bg-[#1C233E] h-screen flex items-center justify-center">
+    <div className="bg-[#1C233E] h-screen flex items-center justify-center box-border">
       <div className="bg-white p-8 rounded shadow-md w-96">
-        <h1 className="text-3xl font-bold mb-4">Todo App</h1>
+        <h1 className="text-3xl font-bold mb-4 text-blue-950 font-mono">
+          Todo App
+        </h1>
 
         <div className="flex mb-4">
           <input
             type="text"
-            className="border rounded-sm p-2 w-[75%] mr-2 bg-slate-100 "
+            className="border rounded-sm p-2 w-[75%] mr-2 bg-slate-100 placeholder:font-mono"
             placeholder="Add new todo"
             value={newTodo}
             onChange={(e) => setNewTodo(e.target.value)}
           />
           <button
-            className="bg-blue-500 text-white px-6 py-2 rounded text-nowrap"
+            className="bg-blue-500 text-white px-6 py-2 rounded text-nowrap font-mono"
             onClick={handleAddTodo}
           >
             Add Todo
@@ -37,11 +41,19 @@ const Form = () => {
           {todos.map((todo, index) => (
             <li
               key={index}
-              className="flex items-center mb-2 justify-between bg-slate-300 p-2 rounded-md"
+              className={`flex items-center mb-2 justify-between bg-slate-300 p-2 rounded-md gap-1 ${
+                todo.isFavorite ? "border-2 border-red-500 bg-pink-200" : ""
+              }`}
             >
-              <span className="mr-2">{todo}</span>
+              <span className="mr-2 basis-[80%] font-mono">{todo.text}</span>
               <button
-                className="bg-red-500 text-white px-2 py-1 rounded"
+                className="text-red-500 px-2 py-1 rounded basis-[10%]"
+                onClick={() => toggleFavorite(index)}
+              >
+                {todo.isFavorite ? <FaHeart /> : <FaRegHeart />}
+              </button>
+              <button
+                className="bg-red-500 text-white px-2 py-1 rounded basis-[10%]"
                 onClick={() => deleteTodo(index)}
               >
                 <FaTrashAlt />
@@ -53,4 +65,5 @@ const Form = () => {
     </div>
   );
 };
+
 export default Form;
--- src/Store.jsx
+++ src/Store.jsx
@@ -2,9 +2,18 @@ import { create } from "zustand";
 
 const useTodoStore = create((set) => ({
   todos: [],
-  addTodo: (newTodo) => set((state) => ({ todos: [...state.todos, newTodo] })),
+  addTodo: (newTodo) =>
+    set((state) => ({
+      todos: [...state.todos, { text: newTodo, isFavorite: false }],
+    })),
   deleteTodo: (index) =>
-    set((state) => ({ todos: state.todos.filter((item, i) => i !== index) })),
+    set((state) => ({ todos: state.todos.filter((items, i) => i !== index) })),
+  toggleFavorite: (index) =>
+    set((state) => {
+      const updatedTodos = [...state.todos];
+      updatedTodos[index].isFavorite = !updatedTodos[index].isFavorite;
+      return { todos: updatedTodos };
+    }),
 }));
 
 export default useTodoStore;
