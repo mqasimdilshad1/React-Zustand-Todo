a953e6f4d515d194b03f3c1c40ce8da43b294754
Muhammad Qasim Dilshad <qasimdilshad336@gmail.com>
2023-12-21 15:06:46 +0500

    background color updated & trash icon added

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,25 +2,25 @@ import useTodoStore from "./Store";
 import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 const Form = () => {
-    const [newTodo, setNewTodo] = useState('');
+  const [newTodo, setNewTodo] = useState("");
   const { todos, addTodo, deleteTodo } = useTodoStore();
 
   const handleAddTodo = (e) => {
     e.preventDefault();
-    if (newTodo.trim() !== '') {
+    if (newTodo.trim() !== "") {
       addTodo(newTodo);
-      setNewTodo('');
+      setNewTodo("");
     }
   };
   return (
-    <div className="bg-gray-200 h-screen flex items-center justify-center">
+    <div className="bg-[#1C233E] h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow-md w-96">
         <h1 className="text-3xl font-bold mb-4">Todo App</h1>
 
         <div className="flex mb-4">
           <input
             type="text"
-            className="border p-2 w-3/4 mr-2"
+            className="border border-blue-950 rounded-sm p-2 w-[75%] mr-2 bg-slate-100 "
             placeholder="Add new todo"
             value={newTodo}
             onChange={(e) => setNewTodo(e.target.value)}
@@ -35,7 +35,10 @@ const Form = () => {
 
         <ul>
           {todos.map((todo, index) => (
-            <li key={index} className="flex items-center mb-2 justify-between">
+            <li
+              key={index}
+              className="flex items-center mb-2 justify-between bg-slate-300 p-2 rounded-md"
+            >
               <span className="mr-2">{todo}</span>
               <button
                 className="bg-red-500 text-white px-2 py-1 rounded"
--- src/Store.jsx
+++ src/Store.jsx
@@ -1,9 +1,10 @@
 import { create } from "zustand";
 
 const useTodoStore = create((set) => ({
-    todos: [],
-    addTodo: (newTodo) => set((state) => ({ todos: [...state.todos, newTodo] })),
-    deleteTodo: (index) => set((state) => ({ todos: state.todos.filter((item,i) => i !== index) })),
-  }));
+  todos: [],
+  addTodo: (newTodo) => set((state) => ({ todos: [...state.todos, newTodo] })),
+  deleteTodo: (index) =>
+    set((state) => ({ todos: state.todos.filter((item, i) => i !== index) })),
+}));
 
-  export default useTodoStore;
+export default useTodoStore;
